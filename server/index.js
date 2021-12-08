ede878dbc1b3525c86d8b9f9d5d60955ee4f9b8c
konchai3343 <watcharin.g43@gmail.com>
2021-12-08 17:17:58 +0700

    1234

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 // server/index.js
 'use strict';
 
-const app = require('./app')
+const app = require('./app');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`)
+  console.log(`App listening on port ${PORT}!`);
 });
